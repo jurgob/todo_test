3a0c715be80313e13c64c8818bfd6471ec85707e
jurgob <jurgo.boemo@gmail.com>
2014-12-18 17:54:03 +0000

    emberjs: reactjs adding

--- emberjs/main.js
+++ emberjs/main.js
@@ -1,93 +1,19 @@
-window.mega_todo = Ember.Application.create();
-
-
-//ROUTING
-mega_todo.Router.map(function() {
-  this.resource('todos', { path: '/' });
-    this.resource('credits', { path: '/credits' });
+require.config({
+    baseUrl: "./",
+    paths: {
+        'jquery': 'http://code.jquery.com/jquery-1.11.1.min',
+        'handlebars':'http://builds.handlebarsjs.com.s3.amazonaws.com/handlebars-v1.3.0',
+        'ember': 'http://builds.emberjs.com/tags/v1.8.1/ember',
+        'ember_data': 'http://builds.emberjs.com/tags/v1.0.0-beta.11/ember-data'
+    },
+    shim: {
+        'ember_data': ['ember'],
+        'ember': {
+            deps: ['jquery','handlebars' ],
+            exports: 'Ember'
+        }
+    },
+    deps: ['mega_todo']
 });
-
-mega_todo.TodosRoute = Ember.Route.extend({
-  model: function() {
-    return this.store.find('todo');
-  }
-});
-
-
-//MODELS
-mega_todo.Todo = DS.Model.extend({
-  text: DS.attr('string'),
-  done: DS.attr('boolean')
-});
-
-
-//FIX DATA
-mega_todo.ApplicationAdapter = DS.FixtureAdapter.extend();
-mega_todo.Todo.FIXTURES = [
- {
-   id: 1,
-   text: 'Dynamic Todo 1',
-   done: true
- },
- {
-   id: 2,
-   text: 'check the checklist 2',
-   done: false
- }
-];
-
-
-//CONTROLLERS
-mega_todo.TodosController = Ember.ArrayController.extend({
-  actions: {
-    createTodo: function() {
-      // Get the todo title set by the "New Todo" text field
-      var title = this.get('newItemText');
-      if (!title.trim()) { return; }
-
-      // Create the new Todo model
-      var todo = this.store.createRecord('todo', {
-        text: title,
-        done: false
-      });
-
-      // Clear the "New Todo" text field
-      this.set('newItemText', '');
-
-      // Save the new model
-      todo.save();
-    }
-  },
-    getCompletedItems: function(){
-        return this.filterBy('done', true).get('length')
-    }.property('@each.done')
-
-});
-
-
-mega_todo.TodoController = Ember.ObjectController.extend({
-  actions:{
-    removeTodo: function () {
-      var todo = this.get('model');
-      todo.deleteRecord();
-      todo.save();
-    }
-  },
-
-  isCompleted: function(key, value){
-    var model = this.get('model');
-
-    if (value === undefined) {
-      // property being used as a getter
-      return model.get('done');
-    } else {
-      // property being used as a setter
-      model.set('done', value);
-      model.save();
-      return !value;
-    }
-  }.property('model.done')
-});
-
 
 
---
+++ emberjs/mega_todo.js
@@ -0,0 +1,95 @@
+define(["ember", "ember_data"], function(Ember){
+    var mega_todo = Ember.Application.create();
+
+
+//ROUTING
+    mega_todo.Router.map(function() {
+        this.resource('todos', { path: '/' });
+        this.resource('credits', { path: '/credits' });
+    });
+
+    mega_todo.TodosRoute = Ember.Route.extend({
+        model: function() {
+            return this.store.find('todo');
+        }
+    });
+
+
+//MODELS
+    mega_todo.Todo = DS.Model.extend({
+        text: DS.attr('string'),
+        done: DS.attr('boolean')
+    });
+
+
+//FIX DATA
+    mega_todo.ApplicationAdapter = DS.FixtureAdapter.extend();
+    mega_todo.Todo.FIXTURES = [
+        {
+            id: 1,
+            text: 'Dynamic Todo 1',
+            done: true
+        },
+        {
+            id: 2,
+            text: 'check the checklist 2',
+            done: false
+        }
+    ];
+
+
+//CONTROLLERS
+    mega_todo.TodosController = Ember.ArrayController.extend({
+        actions: {
+            createTodo: function() {
+                // Get the todo title set by the "New Todo" text field
+                var title = this.get('newItemText');
+                if (!title.trim()) { return; }
+
+                // Create the new Todo model
+                var todo = this.store.createRecord('todo', {
+                    text: title,
+                    done: false
+                });
+
+                // Clear the "New Todo" text field
+                this.set('newItemText', '');
+
+                // Save the new model
+                todo.save();
+            }
+        },
+        getCompletedItems: function(){
+            return this.filterBy('done', true).get('length')
+        }.property('@each.done')
+
+    });
+
+
+    mega_todo.TodoController = Ember.ObjectController.extend({
+        actions:{
+            removeTodo: function () {
+                var todo = this.get('model');
+                todo.deleteRecord();
+                todo.save();
+            }
+        },
+
+        isCompleted: function(key, value){
+            var model = this.get('model');
+
+            if (value === undefined) {
+                // property being used as a getter
+                return model.get('done');
+            } else {
+                // property being used as a setter
+                model.set('done', value);
+                model.save();
+                return !value;
+            }
+        }.property('model.done')
+    });
+
+    return mega_todo;
+
+});
