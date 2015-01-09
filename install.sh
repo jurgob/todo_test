5054c3fc28f6973d89b8b061626db04582a1cbbb
jurgob <jurgo.boemo@gmail.com>
2015-01-09 10:47:01 +0000

    new install on header

---
+++ install.sh
@@ -0,0 +1,13 @@
+npm install
+
+cd angularjs
+[ -f bower.json ] && bower install
+cd ..
+
+cd flux
+[ -f bower.json ] && bower install
+cd ..
+
+cd emberjs
+[ -f bower.json ] && bower install
+cd ..
